d1cf2c7961dce67b39a92b9ded4308b31ea4673d
Bella <bella.kallen2014@gmail.com>
2023-12-10 22:45:07 +0300

    scr added

---
+++ src/main/java/PosterManager.java
@@ -0,0 +1,39 @@
+public class PosterManager {
+    private String[] movies = new String[0];
+    private int defaultMovie;
+
+    public PosterManager(int defaultNumberMovie) {
+        this.defaultMovie = defaultNumberMovie;
+    }
+
+    public PosterManager() {
+        this.defaultMovie = 10;
+    }
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < defaultMovie) {
+            resultLength = movies.length;
+        } else {
+            resultLength = defaultMovie;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
+    }
+}
